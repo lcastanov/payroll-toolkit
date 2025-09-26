9fc6974524e33dc81eb1fd72dccb8e793ffadc4e
Laura <lauracastanovilla@usf.edu>
2025-09-26 11:31:39 -0400

    first commit

---
+++ cc_5.js
@@ -0,0 +1,18 @@
+// Coding Challenge 05
+
+let employees = [
+    {name: 'Lucinda', hourlyRate: 15.95, hoursWorked: 38},
+    {name: 'Max', hourlyRate: 24.72, hoursWorked: 42},
+    {name: 'Elvira', hourlyRate: 13.00, hoursWorked: 40},
+]
+
+// Step 3
+function calculateBasePay(rate, hours) {
+    return (rate * hours);
+}
+
+while (hours <= 40) {
+    calculateBasePay();
+}
+
+console.log(calculateBasePay());
