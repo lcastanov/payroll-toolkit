e677cd5cc2bc14f055ac8e2a15712694dd6d8913
Laura <lauracastanovilla@usf.edu>
2025-09-27 22:27:36 -0400

    finishing off step 3 and finishing step 4

--- cc_5.js
+++ cc_5.js
@@ -1,20 +1,31 @@
 // Coding Challenge 05
 
-let employees = [
-    {name: 'Lucinda', hourlyRate: 15.95, hoursWorked: 38},
+// Step 2
+let employees = 
+    [{name: 'Lucinda', hourlyRate: 15.95, hoursWorked: 38},
     {name: 'Max', hourlyRate: 24.72, hoursWorked: 42},
-    {name: 'Elvira', hourlyRate: 13.00, hoursWorked: 40},
-]
+    {name: 'Elvira', hourlyRate: 13.00, hoursWorked: 40},]
 
 // Step 3
-function calculateBasePay(rate, hours) {
-    return (rate * hours);
+function calculateBasePay(hourlyRate, hoursWorked) {
+    if (hoursWorked <= 40) {
+    return (hourlyRate * hoursWorked);
+    } else {
+    console.log('Base pay cannot be calculated if worked over 40 hours.');
+    }
 }
 
-while (hours <= 40) {
-    calculateBasePay();
-}
+console.log(calculateBasePay(employees[2].hourlyRate, employees[2].hoursWorked));
 
-console.log(calculateBasePay());
 
+// Step 4
+function calculateOvertimePay(hourlyRate, hoursWorked) {
+    if (hoursWorked > 40) {
+    return (hourlyRate * 1.5);
+    } else {
+    console.log('Overtime pay cannot be calculated if worked under 40 hours.');
+    }
+} 
+
+console.log(calculateOvertimePay(employees[0].hourlyRate, employees[0].hoursWorked));
 
