821947fbbc794b2623727f813858a1916125fa1c
Laura <lauracastanovilla@usf.edu>
2025-09-28 22:38:40 -0400

    finalizing step 7

--- cc_5.js
+++ cc_5.js
@@ -6,17 +6,15 @@ let employees =
     {name: 'Max', hourlyRate: 24.72, hoursWorked: 42},
     {name: 'Elvira', hourlyRate: 13.00, hoursWorked: 40},]
 
+
 // Step 3
 function calculateBasePay(hourlyRate, hoursWorked) {
     if (hoursWorked <= 40) {
     return (hourlyRate * hoursWorked);
     } else {
-    console.log('Base pay cannot be calculated if worked over 40 hours.');
     return 0
     }
 }
-
-console.log(calculateBasePay(employees[2].hourlyRate, employees[2].hoursWorked));
 
 
 // Step 4
@@ -24,21 +22,37 @@ function calculateOvertimePay(hourlyRate, hoursWorked) {
     if (hoursWorked > 40) {
     return (hourlyRate * 1.5);
     } else {
-    console.log('Overtime pay cannot be calculated if worked under 40 hours.')
     return 0;
     }
 } 
 
-console.log(calculateOvertimePay(employees[0].hourlyRate, employees[0].hoursWorked));
-
 
 // Step 5
-grossPay = calculateBasePay() + calculateOvertimePay();
-netPay = grossPay * (1 - 0.15)
 function calculateTaxes(grossPay) {
+    netPay = grossPay * (1 - 0.15)
     return netPay.toFixed(2)
 }
 
-console.log(calculateTaxes(grossPay));
 
 // Step 6
+function processPayroll(employee) {
+    basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
+    overtimePay = calculateOvertimePay(employee.hourlyRate, employee.hoursWorked);
+    grossPay = basePay + overtimePay;
+    netPay = calculateTaxes(grossPay);
+
+    let employeePayroll = {
+        name: employee.name, 
+        basePay: basePay,
+        overtimePay: overtimePay,
+        grossPay: grossPay,
+        netPay: netPay,
+    }
+
+    return employeePayroll;
+
+}
+
+// Step 7
+    employees.forEach(employee => {let employeePayroll = processPayroll(employee); console.log(`Employee name: ${employeePayroll.name} |`, 
+        `Base Pay: $${employeePayroll.basePay} |`, `Overtime Pay: $${employeePayroll.overtimePay} |`, `Gross Pay: $${employeePayroll.grossPay} |`, `Net Pay: $${employeePayroll.netPay}`)});
